a9cd133e21fd9fe925a082dcd42d0bb7cf7a2ab4
mudassir45 <mudassirali36@gmail.com>
2023-01-23 16:15:37 +0500

    update config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -18,7 +18,7 @@ task("example", "Example task").setAction(example);
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "^0.7.0",
+    version: "0.7.6",
     settings: {
       optimizer: {
         enabled: true,
